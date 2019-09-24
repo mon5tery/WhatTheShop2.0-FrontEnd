a3f7fe6e6eadd6ab8809ebbfdc6bb31e195ec915
ahalrasheed <ahmed.alrasheed88@gmail.com>
2019-09-24 18:09:09 +0300

    finished functionality

--- components/Profile/ProfilePage.js
+++ components/Profile/ProfilePage.js
@@ -1,11 +1,15 @@
 // if (!this.props.user) return <Redirect to="/login" />;
 
-import { Redirect } from "react-router-dom";
+// import { Redirect } from "react-router-dom";
+import authStore from "../../stores/authStore";
+import { observer } from "mobx-react";
 
 class Profile extends Component {
   render() {
-    if (!this.props.user) return <Redirect to="/login" />;
+    console.log(authStore.user);
+    if (!authStore.user) return this.props.navigation.replace("Login");
+    if (authStore.user) return this.props.navigation.replace("Banak");
   }
 }
 
-export default withNavigation(Profile);
+export default observer(Profile);
