72f201f2de7a8d4bc12d610bb4e5b085c28314b6
mon5tery <ahmed.alrasheed88@gmail.com>
2019-09-25 02:25:19 +0300

    ZzZzZzZzZ

--- components/Profile/ProfilePage.js
+++ components/Profile/ProfilePage.js
@@ -1,8 +1,13 @@
 // if (!this.props.user) return <Redirect to="/login" />;
 
 // import { Redirect } from "react-router-dom";
-import authStore from "../../stores/authStore";
+
+import React, { Component } from "react";
 import { observer } from "mobx-react";
+// NativeBase Components
+import { Card, CardItem, Text, Button, Header, Spinner } from "native-base";
+import authStore from "../../stores/authStore";
+import profileStore from "../../stores/profileStore";
 
 class Profile extends Component {
   componentDidMount = () => {
