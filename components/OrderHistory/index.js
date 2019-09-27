d3db168441124c52be11d2901e1580ac0314d112
mon5tery <ahmed.alrasheed88@gmail.com>
2019-09-27 21:40:48 +0300

    added orderStore

--- components/OrderHistory/index.js
+++ components/OrderHistory/index.js
@@ -3,20 +3,20 @@ import { observer } from "mobx-react";
 
 import { ScrollView } from "react-native";
 //Stores
-import previousOrders from "../../stores/previousOrders";
+import OrderHistory from "../../stores/orderStore";
 //Components
 import Order from "./order";
 
 class Orders extends Component {
   static navigationOptions = () => ({
-    title: "Previous Orders",
+    title: "Order History",
     fontSize: 20
   });
   componentDidMount() {
-    previousOrders.fetchOrders();
+    OrderHistory.fetchOrders();
   }
   render() {
-    const orders = previousOrders.orders.map(order => {
+    const orders = OrderHistory.orders.map(order => {
       return <Order order={order} key={order.id} />;
     });
 
